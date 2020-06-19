2579ef0d8d4d40b69f49f5a8139694fd4e31c8fe
unknown <amr@DESKTOP-7GNJLVB.com>
2020-06-19 22:17:48 +0200

    ?

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::redirect('/', '/admin/login');
+Route::redirect('/', 'en/admin/login');
 Route::group(['prefix' => '{language}'], function () {
     Route::any('/admin/login', "UserController@login");
     Route::get('/admin/dashboard', "UserController@welcome");
